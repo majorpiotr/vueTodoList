fcf5e4e50c6cd2850bcef8edf55fea41225bc00e
majorpiotr <majorpiotr3@gmail.com>
2021-03-04 06:07:17 +0100

    unit tests and url for new api

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 Vue.config.productionTip = false
-Vue.prototype.$hostname = 'http://127.0.0.1:8000/api/';
+Vue.prototype.$hostname = 'http://api.todolist.petermajor.a2hosted.com/api/';
 new Vue({
 	router,
 	render: h => h(App),
